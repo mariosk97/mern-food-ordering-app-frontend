f39cc32d6127c3d27a5cd11a4df99ec439938b9c
Marios <mariosk97@yahoo.gr>
2025-10-16 11:19:13 +0300

    search implemented

---
+++ src/api/RestaurantApi.tsx
@@ -0,0 +1,33 @@
+import type { SearchState } from "@/pages/SearchPage";
+import type { RestaurantSearchResponse } from "@/types";
+import { useQuery } from "@tanstack/react-query";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useSearchRestaurant = (SearchState: SearchState, city?: string) => {
+    const createSearchRequest = async (): Promise<RestaurantSearchResponse> => {
+        const params = new URLSearchParams();
+        params.set("searchQuery", SearchState.searchQuery);
+        params.set("page", SearchState.page.toString());
+        params.set("selectedCuisines", SearchState.selectedCuisines.join(","));
+        params.set("sortOption", SearchState.sortOption);
+        
+        const response = await fetch(
+            `${API_BASE_URL}/api/restaurant/search/${city}?${params.toString()}`
+        );
+
+        if(!response.ok) {
+            throw new Error("Failed to get restaurant");
+        }
+
+        return response.json();
+    }
+
+    const { data: results, isLoading } = useQuery({
+        queryKey: ["searchRestaurants", SearchState],  
+        queryFn: createSearchRequest,
+        enabled: !!city, 
+    });
+
+    return { results, isLoading }
+}
--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -1,18 +1,25 @@
+import { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
-    const { isAuthenticated, isLoading } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
+  const [shouldRender, setShouldRender] = useState(false);
 
-    if (isLoading) {
-        return null;
+  useEffect(() => {
+    if (!isLoading) {
+      const timeout = setTimeout(() => setShouldRender(true), 500);
+      return () => clearTimeout(timeout);
     }
+  }, [isLoading]);
 
-    if(isAuthenticated) {
-        return <Outlet />
-    }
+  if (isLoading || !shouldRender) {
+    return <div className="flex items-center justify-center min-h-screen text-lg">Loading...</div>;
+  }
 
-    return <Navigate to="/" replace />
-}
+  if (isAuthenticated) return <Outlet />;
+
+  return <Navigate to="/" replace />;
+};
 
 export default ProtectedRoute;
